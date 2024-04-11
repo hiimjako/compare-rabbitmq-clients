37be6c32ec54c6104aa701f020381f267ac091de
Alberto Moretti <moretti919@gmail.com>
2024-04-11 14:35:38 +0200

    fix env

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 docker network create rabbitmq-perf-net
-docker run --rm -d --name docker-tc --network host --cap-add NET_ADMIN --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /var/docker-tc:/var/docker-tc lukaszlach/docker-tc
+docker run -d --name docker-tc --network host --cap-add NET_ADMIN --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /var/docker-tc:/var/docker-tc lukaszlach/docker-tc
 
 # creating rabbit server
-docker run --rm --name rabbitmq --network rabbitmq-perf-net -h rabbitmq -p 5552:5552 -p 15672:15672 -d rabbitmq:3.13.1-management-alpine 
+docker run --name rabbitmq --network rabbitmq-perf-net -h rabbitmq -p 5552:5552 -p 15672:15672 -d rabbitmq:3.13.1-management-alpine 
 sleep 10
 docker exec rabbitmq rabbitmq-plugins enable rabbitmq_stream 
 docker exec rabbitmq rabbitmq-plugins enable rabbitmq_management
