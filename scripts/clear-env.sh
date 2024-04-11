31209dd35c9ed57d82b9fc06e981b25ac6bac36f
Alberto Moretti <moretti919@gmail.com>
2024-04-11 12:28:13 +0200

    fix clear env

--- scripts/clear-env.sh
+++ scripts/clear-env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
-docker network remove rabbitmq-perf-net
 docker container stop docker-tc
+docker container rm docker-tc
 docker container stop rabbitmq
+docker container rm rabbitmq
+docker network remove rabbitmq-perf-net
